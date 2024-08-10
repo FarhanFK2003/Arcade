8926669544ec85369058f04b0ea749bfc9e7b546
faiq-ali-24 <121745957+faiq-ali-24@users.noreply.github.com>
2024-08-10 23:14:10 +0500

    Signup_login_page

---
+++ Arcade/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Arcade.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Arcade.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Login() {
+            return View();
+        }
+        public IActionResult Signup() {
+            return View();
+        }
+
+    }
+}
---
+++ Arcade/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Arcade.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
